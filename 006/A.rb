c4126e66d0bf40bf93b748d89726a7064612cbbe
TaigaMikami <mikamimikami60@gmail.com>
2018-02-22 23:25:11 +0900

    006

---
+++ 006/A.rb
@@ -0,0 +1,7 @@
+n = gets.to_i
+
+if n % 3 == 0
+  puts("YES")
+else
+  puts("NO")
+end
---
+++ 006/B.rb
@@ -0,0 +1,8 @@
+n = gets.to_i
+a = [0,0,1]
+
+for i in 3..n-1 do
+  a[i] = (a[i-1] + a[i-2] + a[i-3])%10007
+end
+
+puts(a[n-1])
