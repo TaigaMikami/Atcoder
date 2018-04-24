b01ac706f7495583950e32a1828904eb5a43ebc8
TaigaMikami <mikamimikami60@gmail.com>
2018-04-24 23:04:25 +0900

    There is no special reason

--- 030/B.rb
+++ 030/B.rb
@@ -4,3 +4,4 @@ l = 30*(n + m.to_f/60)
 s = 360 * m.to_f/60
 angle = (l - s).abs
 p [angle, 360-angle].min
+
---
+++ test.rb
@@ -0,0 +1,8 @@
+n = 30
+
+n.times do |i|
+  if 18 * i % 30 == 12
+    puts i
+  end
+end
+
